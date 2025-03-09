bdd9bbb126eb608893f8cc46a6fb12ed140f9e1e
nicholasdoloksaribu <113407564+nicholasdoloksaribu@users.noreply.github.com>
2025-03-09 07:57:56 +0700

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Dashboard Bike Sharing Dataset")
+
+url_day = 'https://github.com/nicholasdoloksaribu/bike-sharing-dashboard/blob/main/data/day.csv';
+url_hour = 'https://github.com/nicholasdoloksaribu/bike-sharing-dashboard/blob/main/data/hour.csv'
+
+# Load dataset
+day_df = pd.read_csv( url_day)
+hour_df = pd.read_csv(url_hour)
+
+st.write("Tampilan Data Awal:")
+st.dataframe(day_df.head())
+
+option = st.selectbox("Pilih Grafik", ["Cuaca vs Penyewaan", "Penyewaan berdasarkan Jam"])
+
+if option == "Cuaca vs Penyewaan":
+    st.bar_chart(day_df.groupby("weathersit")["cnt"].mean())
+
+elif option == "Penyewaan berdasarkan Jam":
+    st.line_chart(hour_df.groupby("hr")["cnt"].mean())
